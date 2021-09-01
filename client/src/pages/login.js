aeceb62cf17f23887e81e6de7c23da8dd6dc8ced
matthew8541 <ytseng35@gatech.edu>
2021-08-31 20:07:25 -0400

    form added

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,8 +1,29 @@
+import { Form, Button } from "react-bootstrap";
+import * as Icon from 'react-bootstrap-icons';
 
 const Login = () => {
   return (
-    <div className="App">
-      <h1>Login</h1>
+    <div>
+      <div className="d-flex justify-content-center align-items-center">
+        <Icon.FilePerson className="me-2" size={40} />
+        <h1>Login</h1>
+      </div>
+      <div className="form">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className="d-flex">Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label className="d-flex">Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
     </div>
   )
 }
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,8 +1,34 @@
+import { Form, Button } from "react-bootstrap";
+import * as Icon from 'react-bootstrap-icons';
 
 const Register = () => {
   return (
-    <div className="App">
-      <h1>Register</h1>
+    <div>
+      <div className="d-flex justify-content-center align-items-center">
+        <Icon.FilePerson className="me-2" size={40} />
+        <h1>Register</h1>
+      </div>
+      <div className="form">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label className="d-flex">Username</Form.Label>
+            <Form.Control type="text" placeholder="Enter username" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className="d-flex">Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label className="d-flex">Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+        </Form>
+      </div>
     </div>
   )
 }
